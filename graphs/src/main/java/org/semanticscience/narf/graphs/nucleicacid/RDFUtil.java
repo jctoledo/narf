7bead5907f7380260bdde931beac76ada64dd9b4
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2013-10-12 09:43:15 -0400

    minor change

--- graphs/src/main/java/org/semanticscience/narf/graphs/nucleicacid/RDFUtil.java
+++ graphs/src/main/java/org/semanticscience/narf/graphs/nucleicacid/RDFUtil.java
@@ -20,8 +20,17 @@
  */
 package org.semanticscience.narf.graphs.nucleicacid;
 
+import java.util.List;
+import java.util.Set;
+
+import org.semanticscience.narf.graphs.lib.cycles.Cycle;
+import org.semanticscience.narf.structures.interactions.BasePair;
+import org.semanticscience.narf.structures.interactions.NucleotideInteraction;
+import org.semanticscience.narf.structures.parts.Nucleotide;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 
 /**
  * @author  Jose Cruz-Toledo
@@ -32,9 +41,20 @@ public class RDFUtil {
 	private static String resource = base.substring(0, base.length()-1)+"_resource:";
 	private static String vocabulary = base.substring(0, base.length()-1)+"_vocabulary:";
 	
-	public static Model createModel(NucleicAcid aNucleicAcid){
+	public static Model createModel(List<Cycle<Nucleotide, InteractionEdge>> acycleList){
 		Model rm = ModelFactory.createDefaultModel();
-		
+		for(Cycle<Nucleotide, InteractionEdge> acyc: acycleList){
+			Nucleotide sv = acyc.getStartVertex();
+			InteractionEdge ie = acyc.getIncomingEdge(sv);
+			
+		}
 		return null;
 	}
+	
+	private static final class Vocab{
+		private static Model m = ModelFactory.createDefaultModel();
+		public static Property rdftype = m.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+		
+	}
+	
 }
